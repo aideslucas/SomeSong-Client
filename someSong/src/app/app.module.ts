d48f34d583cfb3764ab95757fc5cf1f6ec51c326
Lucas <aideslucas@gmail.com>
2017-05-06 13:25:18 +0300

    removed uploadQuestionPage from declarations

--- someSong/src/app/app.module.ts
+++ someSong/src/app/app.module.ts
@@ -14,7 +14,6 @@ import { RegisterPage } from "../pages/register/register";
 import { LoginPage } from '../pages/login/login';
 import { LanguageSelectPage } from '../pages/language-select/language-select';
 import { GenreSelectPage } from '../pages/genre-select/genre-select';
-import { UploadQuestionPage } from "../pages/upload-question/upload-question"
 
 // Ionic Native
 import { StatusBar } from '@ionic-native/status-bar';
@@ -55,8 +54,7 @@ export const firebaseConfig = {
     LoginPage,
     LanguageSelectPage,
     GenreSelectPage,
-    RegisterPage,
-    UploadQuestionPage
+    RegisterPage
   ],
   imports: [
     BrowserModule,
@@ -73,8 +71,7 @@ export const firebaseConfig = {
     LoginPage,
     LanguageSelectPage,
     GenreSelectPage,
-    RegisterPage,
-    UploadQuestionPage
+    RegisterPage
   ],
   providers: [
     Auth,
