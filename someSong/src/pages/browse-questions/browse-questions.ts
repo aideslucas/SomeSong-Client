0e038763375fd686a341166a3fb4d5800503a4d4
klavior <klavior@gmail.com>
2017-05-09 15:29:30 +0300

    added filter by genres

--- someSong/src/pages/browse-questions/browse-questions.ts
+++ someSong/src/pages/browse-questions/browse-questions.ts
@@ -11,12 +11,15 @@ import {Genre} from "../../providers/genre";
 export class BrowseQuestionsPage {
   questions: any;
   user: any;
-  genrs: any;
+  genresDict: any;
+  genresArray: any = [];
+  filteredGenres: any = [];
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private _question: Question,
-              private _genrs: Genre) {
+              private _genres: Genre) {
+
     // If we navigated to this page, we will have an item available as a nav param
     this._question.getAllUnresolvedQuestions().then(data =>
     {
@@ -24,17 +27,64 @@ export class BrowseQuestionsPage {
 
       this.questions = [];
 
-      for (var key in questionsDict) this.questions.push(questionsDict[key]);
+      for (var key in questionsDict) {
+        var question=questionsDict[key];
+        question["enabled"]=true;
+        this.questions.push(question);
+      }
+      console.log(this.questions);
     });
 
-    this._genrs.getGenres().then(data =>{
-      this.genrs = [];
-      var genrsDict = data.val();
-      for (var key in genrsDict) this.genrs.push(genrsDict[key]);
+    this._genres.getGenres().then(data =>{
+      console.log("lalala");
+      this.genresDict = data.val();
+      for (var key in this.genresDict) this.genresArray.push(this.genresDict[key]);
+      console.log(this.genresDict);
     });
-  console.log(this.genrs);
 
   }
+
+  // Functions
+  // TODO: this function should be in utils file.
+  getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+  }
+
+  findOne (haystack, arr) {
+    return arr.some(function (v) {
+    return haystack.indexOf(v) >= 0;
+    });
+  }
+
+  getAllEnabledQuestions(){
+    console.log(this.questions);
+    var enabledQuestions = [];
+    var filteredGenresKeys = this.arrayGenresToKeysGenres(this.filteredGenres);
+    console.log(filteredGenresKeys);
+    for (var i =0; i<this.questions.length; i++){
+      var b = this.findOne(this.questions[i]["genres"],filteredGenresKeys);
+      console.log(this.questions[i]["genres"]);
+      console.log(filteredGenresKeys);
+      console.log(b);
+      if ( !b){
+        enabledQuestions.push(this.questions[i]);
+      }
+    }
+
+    console.log(enabledQuestions);
+    return enabledQuestions;
+  }
+
+  arrayGenresToKeysGenres(genresNamesArray){
+    var genresKeyArray = [];
+    for (var i=0; i<genresNamesArray.length; i++){
+      var genreKey = this.getKeyByValue(this.genresDict, genresNamesArray[i]);
+      genresKeyArray.push(parseInt(genreKey));
+    }
+
+    return genresKeyArray;
+  }
+
   goToQuestion(questionID) {
     this.navCtrl.push(QuestionDetailsPage, questionID);
   }
