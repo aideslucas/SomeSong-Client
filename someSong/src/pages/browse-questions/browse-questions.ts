cac9864024577af00002ba0e504b4e30cebe290d
klavior <klavior@gmail.com>
2017-05-27 16:31:25 +0300

    added search bar to browse questions page

--- someSong/src/pages/browse-questions/browse-questions.ts
+++ someSong/src/pages/browse-questions/browse-questions.ts
@@ -18,6 +18,8 @@ export class BrowseQuestionsPage {
   filteredLanguages: any = [];
   languagesArray: any = [];
   languagesDict: any = [];
+  enabledQuestions: any = [];
+  searchQuery: any = "";
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
@@ -25,22 +27,23 @@ export class BrowseQuestionsPage {
               private _genres: Genre,
               private _languages: Language) {
 
+    var promises = [];
     // If we navigated to this page, we will have an item available as a nav param
-    this._question.getAllUnresolvedQuestions().then(data =>
-    {
+    promises.push(this._question.getAllUnresolvedQuestions().then(data => {
       var questionsDict = data.val();
 
       this.questions = [];
 
       for (var key in questionsDict) {
-        var question=questionsDict[key];
-        question["enabled"]=true;
+        var question = questionsDict[key];
+        question["enabled"] = true;
         this.questions.push(question);
       }
       console.log(this.questions);
-    });
+    }));
 
-    this._genres.getGenres().once('value').then(data =>{
+
+    promises.push(this._genres.getGenres().once('value').then(data => {
       this.genresDict = data.val();
       for (var key in this.genresDict) {
         this.genresArray.push(this.genresDict[key]);
@@ -48,30 +51,28 @@ export class BrowseQuestionsPage {
       console.log(this.genresArray);
       // TODO: change filters to only users preffered genres.
       this.filteredGenres = this.genresArray;
-    });
+    }));
 
-    this._languages.getLanguages().once('value').then(data =>{
+    promises.push(this._languages.getLanguages().once('value').then(data => {
       this.languagesDict = data.val();
       console.log("language dict is - " + this.languagesDict);
-      for ( var key in this.languagesDict) {
+      for (var key in this.languagesDict) {
         this.languagesArray.push(this.languagesDict[key]);
       }
       this.filteredLanguages = this.languagesArray;
-    });
+    }));
 
+    Promise.all(promises).then( values=> {
+        this.getAllEnabledQuestions();
+        console.log(this.questions);
+      }).catch(reason => {
+        console.log(reason);
+    });
   }
+
 
   // Functions
   // TODO: this function should be in utils file.
-  // getKeyByValue(object, value) {
-  //   return Object.keys(object).find(key => object[key] === value);
-  // }
-  //
-  // findOne(haystack, arr) {
-  //   return arr.some(function (v) {
-  //     return haystack.indexOf(v) >= 0;
-  //   });
-  // }
 
   static similarDictAndArrays(arrayOne, arrayTwo){
     for (var key in arrayOne){
@@ -79,31 +80,33 @@ export class BrowseQuestionsPage {
         return true;
       }
     }
-
     return false
   }
 
   getAllEnabledQuestions(){
-    var enabledQuestions = [];
-
+    // var enabledQuestions = [];
+    this.enabledQuestions = [];
     for (var i =0; i<this.questions.length; i++){
       console.log("genres of question - " + this.questions[i]['genres']);
       if (BrowseQuestionsPage.similarDictAndArrays(this.questions[i]['genres'],this.filteredGenres) &&
           BrowseQuestionsPage.similarDictAndArrays(this.questions[i]['languages'],this.filteredLanguages)){
-        enabledQuestions.push(this.questions[i]);
+        this.enabledQuestions.push(this.questions[i]);
       }
     }
-    return enabledQuestions;
-  }
 
-  // valuesArrayToKeysArray(array, dict){
-  //   var keysArray = [];
-  //   for (var i=0; i<array.length; i++){
-  //     var genreKey = this.getKeyByValue(dict, array[i]);
-  //     keysArray.push(parseInt(genreKey));
-  //   }
-  //   return keysArray;
-  // }
+    if (!this.searchQuery) {
+      return this.enabledQuestions;
+    }
+
+    console.log("Query is ----------- " + this.searchQuery);
+
+    this.enabledQuestions = this.enabledQuestions.filter((v) => {
+      if(v.title && this.searchQuery) {
+        return v.title.toLowerCase().indexOf(this.searchQuery.toLowerCase()) > -1;
+      }
+    });
+    return this.enabledQuestions;
+  }
 
   goToQuestion(questionID) {
     this.navCtrl.push(QuestionDetailsPage, questionID);
